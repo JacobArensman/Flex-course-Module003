bd2738900ddc85a58b0775f282a0b794ca987d02
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-08 15:52:40 +0200

    Add files via upload

---
+++ module003-opdracht002/index.js
@@ -0,0 +1,9 @@
+//Dit is een grote som
+/*
+function keer() { return (1230941* 1823794); } 
+        console.log(keer());
+
+
+ function deel() { return (637263/ 54); } 
+        console.log(deel());
+*/
