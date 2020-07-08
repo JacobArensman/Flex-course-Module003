ae6243f1c7f298fa2b531ed378cfc0a68f46244b
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-08 16:00:52 +0200

    Add files via upload

---
+++ module003-opdracht003/script.js
@@ -0,0 +1,32 @@
+//Is de persoon boven de 18
+const myAge = 29; 
+
+if (myAge > 18){
+    document.write("je mag naar binnen");
+}
+
+
+//Is het een vrouw
+const isFemale = true;
+
+if (isFemale == true){
+    document.write("je mag naar binnen mevrouw");
+}
+
+else if (isFemale == false){
+    document.write("geen worsten feestje");
+}
+
+
+//is er een bob
+const driverStatus = bob;
+
+if (driverStatus == bob){
+    document.write("rij veilig");
+} 
+
+else {
+    document.write("bel een taxi");
+}
+
+console.log(driverStatus);
