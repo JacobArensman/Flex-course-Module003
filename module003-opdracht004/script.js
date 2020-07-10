06ebf052ade3ea60bf0c1321e4ad72ec94670f4b
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-10 11:16:59 +0200

    Add files via upload

---
+++ module003-opdracht004/script.js
@@ -0,0 +1,39 @@
+
+
+let myVar = 26;
+
+if (myVar >= 18 && myVar <= 25) {
+    document.write("50% korting!")
+} else {
+    document.write("Kom binnen!")
+}
+
+
+
+
+
+const name = Sarah;
+if (name == Sarah || name == Abraham){
+    document.write("gratis biertje");
+}
+
+else if (name != Sarah || name != Abraham){
+    document.write("geen gratis pilsie");
+}
+
+
+
+
+
+
+const totalAmount = 100;
+
+if (totalAmount > 25 && totalAmount < 50) {
+    document.write("Je krijgt gratis vega bitterballen mee naar huis!");
+
+} else if (totalAmount > 50 && totalAmount < 100){
+    document.write("Je krijgt gratis nachos mee naar huis!");
+
+} else if (totalAmount > 100 && totalAmount < 1000){
+    document.write("Je krijgt gratis champagne mee naar huis!");
+}
