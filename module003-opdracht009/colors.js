cfe01f19fc77eda940404a8115c6e75b98f7ce56
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-12 12:11:42 +0200

    Update colors.js

--- module003-opdracht009/colors.js
+++ module003-opdracht009/colors.js
@@ -1,26 +1,38 @@
-var colors = ["yellow, blue, red, orange"];
-var i = 0;
-var text = "";
-for (;colors[i];) {
-  text += colors[i];
+const myColorArray = ["yellow", "blue", "red", "orange"];
+
+// oude manieren
+let i = 0;
+while (i < myColorArray.length) {
+  console.log(myColorArray[i]);
   i++;
 }
 
-while (colors[i]) {
-  text += colors[i];
-  i++;
+for (let i = 0; i < myColorArray.length; i++) {
+  console.log(myColorArray[i]);
 }
 
+// nieuwe manier met forEach
+myColorArray.forEach((color) => console.log(color));
 
+// 1. Mijn for loop is 3 regels, mijn while loop is 5 regels
+// 2. ForEach method is (met een arrow function), 1 regel.
+// 3. Een array method heeft een aantal voordelen:
+// --> je kunt niet meer verstrikt raken in een infinite loop.
+// --> je hoeft niet meer bij te houden met een i, bij welke iteratie je bent.
+// --> je hebt direct toegang tot het item waar je overheen loopt dmv het argument en de callback-functie die je de array-method geeft als argument.
+// --> de naam "forEach" zegt ook meteen wat je gaat doen en is semantischer dan een while of for loop.
 
-const array1 = ['a', 'b', 'c'];
+const myColors = {
+  colorWall: "blue",
+  colorFruit: "orange",
+  colorCar: "red",
+  colorHair: "white",
+  colorGras: "green",
+};
 
-array1.forEach(element => console.log(element));
+for (x in myColors) {
+  console.log(myColors[x]);
+}
 
-//1. = 7
-//2. = 2
-//3. = leesbaarder, voor mij persoonlijker ook logischer. Dit komt omdat je niet 
-//alleen maar nummers ziet maar ook de termen voor alle properties vna de objecten.
-//dit mak tdat het makkerlijker terug te lezen valt en daardoor makkelijker te herbruiken.
-// daarnaast is het ook minder code dus makkelijker te zien en bergijpen. 
-// 
+// Ja, je bent nu aan het itereren. Echter ben je aan het itereren over een OBJECT.
+// Array Methods zijn NIET beschikbaar op een object.
