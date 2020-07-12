3d18531bf9e6b2e6a7ac1622a016ee696db75a81
JacobArensman <66920417+JacobArensman@users.noreply.github.com>
2020-07-12 12:09:54 +0200

    Add files via upload

---
+++ module003-opdracht009/colors.js
@@ -0,0 +1,26 @@
+var colors = ["yellow, blue, red, orange"];
+var i = 0;
+var text = "";
+for (;colors[i];) {
+  text += colors[i];
+  i++;
+}
+
+while (colors[i]) {
+  text += colors[i];
+  i++;
+}
+
+
+
+const array1 = ['a', 'b', 'c'];
+
+array1.forEach(element => console.log(element));
+
+//1. = 7
+//2. = 2
+//3. = leesbaarder, voor mij persoonlijker ook logischer. Dit komt omdat je niet 
+//alleen maar nummers ziet maar ook de termen voor alle properties vna de objecten.
+//dit mak tdat het makkerlijker terug te lezen valt en daardoor makkelijker te herbruiken.
+// daarnaast is het ook minder code dus makkelijker te zien en bergijpen. 
+// 
